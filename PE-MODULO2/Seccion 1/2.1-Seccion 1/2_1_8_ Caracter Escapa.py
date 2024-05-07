d7000ac4878245a669c89a66ce799f234ba88128
adrianpyth <azc7794@gmail.com>
2024-05-07 16:21:26 -0600

    Agregue primeras secciones del modulo 2

---
+++ PE-MODULO2/Seccion 1/2.1-Seccion 1/2_1_8_ Caracter Escapa.py
@@ -0,0 +1,6 @@
+#Se hace uso del carater \n para hacer un salto del linea de caracteres de texto 
+#Si se utiliza print() sin ningun argumenton dentro valga la redundancia esto ejecutara una linea vacio en la terminal/consola
+#\n viene de la palabra newline5
+print("Adrian\n Zuniga") 
+print() #Esto imprimira una linea vacia
+print("Castro")
---
+++ PE-MODULO2/Seccion 1/2.1-Seccion 1/2_1_9 Multiples Argumentos.py
@@ -0,0 +1,12 @@
+print("Witsi", "Arana", " Columpio")
+
+# La función print() invocada con más de un argumento los muestra todos en una sola línea.
+# La función print() pone un espacio entre los argumentos de salida por iniciativa propia.
+#Para convertir todas las lineas de texto seleccionas en comentarios se utiliza crtl+k y luego crtl + c
+
+# La forma en que estamos pasando los argumentos a la función print() 
+# es la más común en Python, y se llama la forma posicional. 
+# Este nombre proviene del hecho de que el significado del argumento está 
+# dictado por su posición (por ejemplo, el segundo argumento se mostrará después del primero, no al revés).
+print("Witsi", "Arana", " Columpio")
+print("La madre que los pario")
